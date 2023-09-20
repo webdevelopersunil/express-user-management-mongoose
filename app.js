c11ff8ebe869b968803f46c3fda507c0ca31be32
webdevsunil <webdevelopersunil>
2023-09-20 11:47:35 +0530

    Node

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 
+// require the path module from the express
+const path = require('path');
 
+// Set the port for runnung the server
 const port = 8000;
 
-
+// calling express here as a function, This app function will have the all functionality which needed to run a Server.
 const app = express();
+
+// Middleware for static files like (css,js,images and js libraries)
+app.use(express.static('assets'));
+
+// set the deffault template engine
+app.set('view engine','ejs');
+
+// setting here the views folder path
+app.set('views', path.join(__dirname,'views'));
 
 
 app.get('/', (req, res) => {
-    res.send('<h2>sd</h2>');
+    return res.render('./homepage');
 })
-
 
 
 
